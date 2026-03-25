d40c0ae9d8bd13cec4631c0104c25288d5318a70
Bhargav-611 <bhargav.chalodiya611@gmail.com>
2026-03-26 00:45:25 +0530

    fix mongo uri

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log('MongoDB Connected');
+        await mongoose.connect(process.env.MONGO_URI, {
+            dbName: "plmDB",
+        });
+
+        console.log("MongoDB connected");
     } catch (err) {
         console.error(err.message);
         process.exit(1);
